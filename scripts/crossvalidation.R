cf4a47d467f9386f54d4792dc461e8b0b9ef49ae
Mario Castro <mariocastro73@gmail.com>
2020-09-25 12:59:02 +0200

    Changes

--- scripts/crossvalidation.R
+++ scripts/crossvalidation.R
@@ -1,7 +1,7 @@
 library(MLTools)
 library(caret)
 
-# Let's play a little bit with knn and logistic-regression
+# Let's play a little bit with knn
 data <- read.csv('https://raw.githubusercontent.com/mariocastro73/ML2020-2021/master/datasets/data-for-knn.csv')
 set.seed(1234)
 train.idx <- createDataPartition(data$Y,p=0.8,list=FALSE)
@@ -20,6 +20,7 @@ Plot2DClass(data.trn[,1:2], #Input variables of the model
             fit.knn,#Fitted model with caret
             var1 = "X1", var2 = "X2",
             selClass = "YES")
+
 pred.knn <- predict(fit.knn,data.tst)
 cm.knn <- table(data.tst$Y,pred.knn)
 confusionMatrix(cm.knn)
@@ -53,26 +54,6 @@ plot(fit.knn.cv)
 plot(fit.knn.cv,metric = "Specificity")
 plot(fit.knn.cv,metric = "Sensitivity")
 plot(fit.knn.cv,metric = "Kappa")
-
-# Logistic regression
-fit.lr <- train(Y ~ ., data = data.trn, method = "glm",
-                    family=binomial(),
-                    preProcess = c("center","scale"))
-print(fit.lr)
-
-ctrl  <- trainControl(method  = "cv",number  = 10)
-fit.lr.cv <- train(Y ~ ., data = data.trn, method = "glm",
-                    family=binomial(),
-                    trControl = ctrl, 
-                    preProcess = c("center","scale"), 
-                    tuneLength = 30) # Try 30 (odd) values
-print(fit.lr.cv)
-
-Plot2DClass(data.trn[,1:2], #Input variables of the model
-            data.trn$Y,     #Output variable
-            fit.lr.cv,#Fitted model with caret
-            var1 = "X1", var2 = "X2",
-            selClass = "YES")
 
 # Partition tree
 ctrl  <- trainControl(method  = "cv",number  = 10)
