b4994068e6056e2c2d60f6613a40a8361102c52b
Mario Castro <mariocastro73@gmail.com>
2020-10-26 13:53:31 +0100

    Changes

--- scripts/moving-average-loess.R
+++ scripts/moving-average-loess.R
@@ -1,6 +1,4 @@
 require(smooth)
-library(ggpubr)
-library(ggplot2)
 
 ########################################################
 set.seed(999)
@@ -13,6 +11,7 @@ plot(data)
 sma(data$Output,order=2,h=0,silent=FALSE) # 2 day average
 sma(data$Output,order=7,h=0,silent=FALSE) # 7 day average
 sma(data$Output,order=14,h=0,silent=FALSE) # 14 day average
+sma(data$Output,order=7,h=14,silent=FALSE,interval='p') # 14 day average
 
 # Exponential rolling average
 alpha <- .5
@@ -50,44 +49,4 @@ fit <- loess(wage~age,Wage)
 new <- data.frame(age=seq(10,90,length=100))
 pred <- predict(fit,new)
 lines(new$age,pred,col=2,lwd=3)
-
-#################################################################
-# ggplot do all this stuff for us with the function "geom_smooth"
-#################################################################
-
-# Linear regression
-gg.lm <- ggplot(data,aes(x=Predictor,y=Output))+geom_point()+geom_smooth(method='lm')
-print(gg.lm)
-# Quadratic regression
-gg.qua <- ggplot(data,aes(x=Predictor,y=Output))+geom_point()+
-  geom_smooth(method='lm',formula= y ~ poly(x,2))
-print(gg.qua)
-# Cubic regression
-gg.cub <- ggplot(data,aes(x=Predictor,y=Output))+geom_point()+
-  geom_smooth(method='lm',formula= y ~ poly(x,3))
-print(gg.cub)
-# Loess
-gg.lo <- ggplot(data,aes(x=Predictor,y=Output))+geom_point()+
-  geom_smooth(method='loess')
-# Automatic GAM
-gg.gam <- ggplot(data,aes(x=Predictor,y=Output))+geom_point()+
-  geom_smooth(method='gam')
-print(gg.gam)
-# Natural splines
-gg.ns2 <- ggplot(data,aes(x=Predictor,y=Output))+geom_point()+
-  geom_smooth(method='gam',formula=y ~ splines::ns(x, 2)) 
-print(gg.ns2)
-gg.ns3 <- ggplot(data,aes(x=Predictor,y=Output))+geom_point()+
-  geom_smooth(method='gam',formula=y ~ splines::ns(x, 3)) 
-print(gg.ns3)
-gg.bs2 <- ggplot(data,aes(x=Predictor,y=Output))+geom_point()+
-  geom_smooth(method='gam',formula=y ~ splines::bs(x, 2)) 
-print(gg.bs2)
-gg.bs30 <- ggplot(data,aes(x=Predictor,y=Output))+geom_point()+
-  geom_smooth(method='gam',formula=y ~ splines::bs(x, 30)) 
-print(gg.bs30)
-
-x11(width = 16,height = 10)
-ggarrange(gg.lm,gg.qua,gg.cub,gg.lo,gg.gam,gg.ns2,gg.ns3,gg.bs2,gg.bs30,
-          labels = c("Linear", "Quadratic", "Cubic","LOESS","GAM","NatSpline2","NatSpline3","Spline2","Spline3"))
 
