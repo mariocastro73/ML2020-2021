ba48bd7dd4d72b7c0f4f9b9847d96c59980e4793
Mario Castro <mariocastro73@gmail.com>
2020-11-09 09:29:24 +0100

    Changes

--- scripts/random-walks.R
+++ scripts/random-walks.R
@@ -15,12 +15,3 @@ autoplot(goog200,series="Real") + autolayer(fake.goog200,series="Fake data")
 
 set.seed(12345)
 autoplot(ts(cumsum(rnorm(1000))))
-
-# Stock market: noise plus unexpected "jump" events
-y <- diff(goog200)
-y2 <- y[-165]
-sd(y2)
-jump <- y[165]/sd(y2)
-autoplot(goog200,series="Google stock prices",col=1) +
-autolayer(ts(c(goog200[165],goog200[166]),start=165),series=sprintf("A %.0f sigma event!",jump))
-
