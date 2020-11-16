7c8778195af2d48eaf7b22ef0ceb1e4473326b9b
Mario Castro <mariocastro73@gmail.com>
2020-11-16 21:17:24 +0100

    Changes

--- scripts/simLTF.R
+++ scripts/simLTF.R
@@ -31,6 +31,45 @@ simLTF <- function(x,omega=1,delta=NULL,b=0){
            filter = omega,
            method = 'convolution',
            sides = 2)
-  xfiltTOT <- xfilt
-  return(ts(xfiltTOT))
+  return(ts(xfilt))
 }
+
+#########################################################
+# Mario Castro Ponce
+# Mon Nov 16 15:08:26 2020 ------------------------------
+#########################################################
+library(fpp2)
+library(Hmisc)
+library(quantmod)
+
+sim.LTF.ARIMA <- function(x,omega=1,delta=NULL,b=0,p=NULL,d=0,q=NULL){
+  #x <- input time series
+  #omega <- vector with numerator coefficient values
+  #delta <- vector with denominator coefficient values
+  #b <- lag value
+  
+  x <- Lag(x,b)
+  x[is.na(x)]<-0
+  
+  # AR regression terms
+  if (!is.null(delta)) {
+    xfilt <-
+      filter(x,
+             filter = delta,
+             method = 'recursive',
+             sides = 1)
+  } else {
+    xfilt <- x # In case there are no -AR coefficients
+  }
+  # MA regression terms
+  xfilt <-
+    filter(xfilt,
+           filter = omega,
+           method = 'convolution',
+           sides = 2)
+
+    noise <- arima.sim(model = list(order=c(length(p),d,length(q))),n=length(x))
+  y <- as.numeric(xfilt+noise)
+  return(ts(y))
+}
+
